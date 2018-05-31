2901019f03d62a256d2432511b484f2a6707bd74
Sylvain Hamelain <sylvain.hamelain@blablacar.com>
2018-05-31 17:26:24 +0200

    First API call

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 #!/usr/bin/env node
-const redis = require('redis').createClient(process.env.REDIS_URL);
+const redis = require('redis').createClient(process.env.REDIS_URL)
+var request = require("request")
 
-const createSlackEventAdapter = require('@slack/events-api').createSlackEventAdapter;
-const slackEvents = createSlackEventAdapter(process.env.SLACK_TOKEN);
-const port = process.env.PORT || 3000;
+var options = { method: 'GET',
+  url: 'https://slack.com/api/channels.history',
+  qs:
+   { token: process.env.SLACK_TOKEN,
+     channel: 'C027VGR1H' },
+  headers:
+   { 'cache-control': 'no-cache',
+     'content-type': 'application/x-www-form-urlencoded' } }
+
+request(options, (error, response, body) => {
+  if (error) {
+      throw new Error(error)
+  }
+
+  console.log(body)
+})
+
+const createSlackEventAdapter = require('@slack/events-api').createSlackEventAdapter
+const slackEvents = createSlackEventAdapter(process.env.SLACK_TOKEN)
+const port = process.env.PORT || 3000
 
 slackEvents.on('reaction_added', (event) => {
   console.log(`Received a reaction_added event: ${JSON.stringify(event)}`);
-});
+})
 
 slackEvents.on('reaction_removed', (event) => {
-  console.log(`Received a reaction_removed event: ${JSON.stringify(event)}`);
-});
+  console.log(`Received a reaction_removed event: ${JSON.stringify(event)}`)
+})
 
-slackEvents.on('error', console.error);
+slackEvents.on('error', console.error)
 
 // Start a basic HTTP server
 slackEvents.start(port).then(() => {
-  console.log(`server listening on port ${port}`);
-});
+  console.log(`server listening on port ${port}`)
+})
 
-const app = require('express');
+const app = require('express')
 app.get('/', async (req, res) => {
-  res.send('Hello world!');
-});
-app.listen(80);
-console.log(`Listening on port 80...`);
+  res.send('Hello world!')
+})
+app.listen(80)
+console.log(`Listening on port 80...`)

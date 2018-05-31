7c1aeb80602b46305bf34c1c02f114022506d393
Thomas Bailly-Salins <thomasbaillysalins01@gmail.com>
2018-05-31 22:52:20 +0200

    database model refactored

--- redis.js
+++ redis.js
@@ -2,23 +2,24 @@ const redis = require('redis')
 const { promisify } = require('util')
 
 const client = redis.createClient(process.env.REDIS_URL)
-const getAsync = promisify(client.get).bind(client)
-const setAsync = promisify(client.set).bind(client)
-const getKeysAsync = promisify(client.keys).bind(client)
+const zscoreAsync = promisify(client.zscore).bind(client)
+const zaddAsync = promisify(client.zadd).bind(client)
+const zrevrangeAsync = promisify(client.zrevrange).bind(client)
+const zrevrangebyscoreAsync = promisify(client.zrevrangebyscore).bind(client)
 
 client.on('error', (err) => {
   console.log(`Error: ${err}`)
 })
 
 function userEarnPoints(user, points) {
-  return getAsync(user)
+  return zscoreAsync('leaderboard', user)
     .then((res) => {
-    	let parsedRes = JSON.parse(res);
-      let totalScore = 0
+      const parsedRes = JSON.parse(res)
+      let score = 0
       if (parsedRes !== null) {
-        totalScore = parsedRes.score + points
+        score = parsedRes.score + points
       }
-      return (setAsync(user, JSON.stringify({ score: totalScore })))
+      return (zaddAsync('leaderboard', user, score))
     }).then((res) => {
       console.log(`User points successfully set - ${res}`)
     }).catch((err) => {
@@ -26,18 +27,21 @@ function userEarnPoints(user, points) {
     })
 }
 
+function getLeaderBoard(numberOfUsers) {
+  return zrevrangeAsync('leaderboard', 0, numberOfUsers)
+    .then((res) => {
+      console.log(JSON.stringify(res))
+    })
+}
+
 function displayDatabase() {
-  return getKeysAsync('*').then((keys) => {
-    for (let i = 0; i < keys.length; i += 1) {
-      getAsync(keys[i]).then((values) => {
-        console.log(`${keys[i]} have ${values} points`)
-      }).catch((err) => {
-        console.error(err)
-      })
-    }
+  return zrevrangebyscoreAsync('+inf', -1).then((res) => {
+    console.log(JSON.stringify(res))
   }).catch((err) => {
     console.error(err)
   })
 }
+
+getLeaderBoard(3)
 
 module.exports = { userEarnPoints, displayDatabase }

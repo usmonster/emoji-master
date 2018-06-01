0238c66dc80860f8c607efacddb2ef4fe8ffd0f7
usmonster <akeju00+github@gmail.com>
2018-06-01 11:05:14 +0200

    lint

--- bot.js
+++ bot.js
@@ -4,21 +4,36 @@ const { getLeaderBoard } = require('./redis')
 const url = process.env.SLACK_WEBHOOK_URL
 const webhook = new IncomingWebhook(url)
 
+function getMessageObj(res) {
+  const message = `#LEADERBOARD
+  :hamburger: *${res[0].name}* with a score of ${res[0].score}
+  :fries: *${res[1].name}* with a score of ${res[1].score}
+  :cup_with_straw: *${res[2].name}* with a score of ${res[2].score}
+  `
+
+  const messageObj = {
+    text: message,
+    mrkdwn: true,
+  }
+
+  return messageObj
+}
+
 async function emojiMasterCommand() {
-  let res = await getLeaderBoard(3)
-  let messageObj = getMessageObj(res)
-  
+  const top3 = await getLeaderBoard(3)
+  const messageObj = getMessageObj(top3)
+
   webhook.send(messageObj, (err, res) => {
-      if (err) {
-          console.log('Error:', err)
-      } else {
-          console.log('Message sent: ', res)
-      }
-  });
+    if (err) {
+      console.log('Error:', err)
+    } else {
+      console.log('Message sent: ', res)
+    }
+  })
 
   // getLeaderBoard(3).then((res) => {
   //   let messageObj = getMessageObj(res)
-    
+
   //   webhook.send(messageObj, (err, res) => {
   //       if (err) {
   //           console.log('Error:', err)
@@ -27,21 +42,6 @@ async function emojiMasterCommand() {
   //       }
   //   });
   // })
-}
-
-function getMessageObj(res) {
-	const message = `#LEADERBOARD
-  :hamburger: *${res[0].name}* with a score of ${res[0].score}
-  :fries: *${res[1].name}* with a score of ${res[1].score}
-  :cup_with_straw: *${res[2].name}* with a score of ${res[2].score}
-  `
-
-	const messageObj = {
-		'text': message,
-		'mrkdwn': true
-	}
-
-	return messageObj;
 }
 
 module.exports = { emojiMasterCommand }
